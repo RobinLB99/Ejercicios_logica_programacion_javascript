0c9502b6b343d616648a500f0e28a39d32edd106
RobinLB99 <RJLB991102@hotmail.com>
2023-03-15 14:54:42 -0500

    Ejercicios propuestos por Bing

---
+++ Ejercicios Arrays y Strings/script.js
@@ -0,0 +1,35 @@
+// Aquí hay una lista de ejercicios con un estilo de prueba técnica para practicar arrays y sus métodos en JavaScript:
+
+// 1. Dado un array de números enteros, escribe una función que devuelva el segundo elemento más grande del array.
+
+
+// 2. Dado un array de cadenas, escribe una función que devuelva la cadena más larga del array.
+
+
+// 3. Dado un array de números enteros y un número entero n, escribe una función que devuelva el número de veces que n aparece en el array.
+
+
+// 4. Dado un array de números enteros y un número entero k, escribe una función que devuelva true si hay dos elementos en el array cuya suma sea igual a k o false si no los hay.
+
+
+//Aquí hay una lista de ejercicios con un estilo de prueba técnica para practicar strings y sus métodos en JavaScript:
+
+
+// 1. Dada una cadena, escribe una función que devuelva la cantidad de vocales en la cadena.
+
+
+
+// 2. Dada una cadena, escribe una función que devuelva la cantidad de palabras en la cadena (separadas por espacios).
+
+
+// 3. Dada una cadena y un carácter específico, escribe una función que devuelva cuántas veces aparece ese carácter en la cadena.
+
+
+
+// Finalmente, aquí hay algunos ejercicios con un estilo de prueba técnica que combinan tanto arrays como strings:
+
+
+// 1. Dado un array de cadenas y un carácter específico, escribe una función que devuelva cuántas veces aparece ese carácter en todas las cadenas del array.
+
+
+// 2. Dado un string y dos caracteres específicos a y b, escribe una función que reemplace todas las apariciones del carácter a por el carácter b.
