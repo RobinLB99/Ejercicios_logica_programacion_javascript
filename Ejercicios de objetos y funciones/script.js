ab28f04656ba0c5469b46c2d20051bec6cac6230
RobinLB99 <RJLB991102@hotmail.com>
2023-03-17 14:28:47 -0500

    Ejercicios de objetos y funciones

--- Ejercicios de objetos y funciones/script.js
+++ Ejercicios de objetos y funciones/script.js
@@ -1,52 +1,62 @@
 /* Ejercicios de objetos en JavaScript: -------------------------------------------*/
-const coche = {
-    marca: "Toyota",
-    modelo: "Corolla",
-    año: 2020,
-    color: "rojo",
-    kilometraje: 15000,
-    encendido: false,
-    arrancar: function() {
-        if (this.encendido) {
-            console.log("El coche ya está encendido.");
-        } else {
-            this.encendido = true;
-            console.log("El coche se ha encendido.");
-        }
-    },
-    apagar: function() {
-        if (this.encendido) {
-            this.encendido = false;
-            console.log("El coche se ha apagado.");
-        } else {
-            console.log("El coche ya está apagado.");
-        }
-    },
-    conducir: function(km) {
-        if (this.encendido) {
-            this.kilometraje += km;
-            console.log("Has conducido " + km + " kilómetros. El coche tiene ahora " + this.kilometraje + " kilómetros.");
-        } else {
-            console.log("No puedes conducir si el coche está apagado. Arráncalo primero.");
-        }
-    }
-};
 
 // 1. Dado un objeto con propiedades y valores, escribe una función que devuelva un array con todas las claves del objeto.
 
 const clavesObjeto = obj => Object.keys(obj)
 
 console.log(clavesObjeto(coche));
+console.log(clavesObjeto(libro));
+console.log(clavesObjeto(reloj));
+
 
 // 2. Dado un objeto con propiedades y valores, escribe una función que devuelva un array con todos los valores del objeto.
 
+const allValuesObject = (objeto) => Object.values(objeto)
+
+console.log(allValuesObject(coche));
+console.log(allValuesObject(libro));
+console.log(allValuesObject(reloj));
+
+
 // 3. Dado un objeto y una clave específica, escribe una función que devuelva el valor asociado a esa clave en el objeto.
+
+const valueKeyObject = (object, key) =>
+    object.hasOwnProperty(key) ? object[key] : "No la propiedad en el objecto"
+
+console.log(valueKeyObject(coche, "color"));
+console.log(valueKeyObject(reloj, "marca"));
+console.log(valueKeyObject(libro, "titulo"));
 
 
 /* Ejercicios de funciones en JavaScript: -------------------------------------------*/
 
 // 1. Escribe una función que tome dos números como argumentos y devuelva su suma.
 
+const suma = (a, b) => a + b
+
+console.log(suma(5, 8));
+
 // 2. Escribe una función que tome un número como argumento y devuelva su factorial (n!).
+// const factorial = (num) => {
+//     let factorial = num;
+//     for (let i = num - 1; i >= 1; i--) {
+//         factorial *= i
+//     }
+//     return factorial
+// }
+
+const factorial = (num) => num === 0 ? 1 : num * factorial(num - 1)
+
+console.log(factorial(8));
 
 // 3. Escribe una función que tome un número como argumento y devuelva true si el número es primo o false si no lo es.
+
+const primo = (num) => {
+    if (num <= 1) return 'No es primo';
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return 'No es primo';
+    }
+    return 'Es primo';
+}
+
+console.log(primo(28));
