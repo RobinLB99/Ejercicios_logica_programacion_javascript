bb8149410d5aaadbfc0557edef08d4fb4db14098
RobinLB99 <RJLB991102@hotmail.com>
2023-03-25 00:16:20 -0500

    Ejercicios de asincronia

---
+++ Ejercicios de Asincronia/main.js
@@ -0,0 +1,61 @@
+// Aquí hay una lista de 20 ejercicios básicos para practicar asincronía con JavaScript: ----------------------------------------------------------------------
+
+// Crear una función que devuelva una promesa que se resuelva después de un tiempo determinado.
+
+
+// Crear una función que devuelva una promesa que se rechace después de un tiempo determinado.
+
+
+// Crear una función que use setTimeout para imprimir un mensaje después de un tiempo determinado.
+
+
+// Crear una función que use setInterval para imprimir un mensaje cada cierto tiempo.
+
+
+// Crear una función que use Promise.all para esperar a que varias promesas se resuelvan.
+
+
+// Crear una función que use Promise.race para esperar a que la primera de varias promesas se resuelva.
+
+
+// Crear una función que use async/await para esperar a que una promesa se resuelva.
+
+
+// Crear una función que use async/await y un bucle for para esperar a que varias promesas se resuelvan en secuencia.
+
+
+// Crear una función que use async/await y Promise.all para esperar a que varias promesas se resuelvan en paralelo.
+
+
+// Crear una función que use fetch para obtener datos de una API y luego procese los datos recibidos.
+
+
+// Crear una función que use XMLHttpRequest para obtener datos de una API y luego procese los datos recibidos.
+
+
+// Crear una función que maneje errores al obtener datos de una API usando fetch.
+
+
+// Crear una función que maneje errores al obtener datos de una API usando XMLHttpRequest.
+
+
+// Crear una función que cancele una solicitud en curso usando AbortController.
+
+
+// Crear un ejemplo de código que muestre cómo usar Promise.resolve.
+
+
+// Crear un ejemplo de código que muestre cómo usar Promise.reject.
+
+
+// Crear un ejemplo de código que muestre cómo encadenar varias promesas usando .then
+
+
+// Crear un ejemplo de código que muestre cómo manejar errores en una cadena de promesas usando .catch
+
+
+// Crear un ejemplo de código que muestre cómo usar el método .finally en una cadena de promesas.
+
+
+// Crear un ejemplo de código que muestre cómo crear y usar un objeto Promise.
+
---
+++ Ejercicios de asincronia 2/main.js
@@ -0,0 +1,59 @@
+// Crear una promesa que resuelva después de un tiempo determinado utilizando setTimeout().
+
+
+// Crear una función asincrónica que llame a una API y devuelva los resultados en formato JSON.
+
+
+// Crear una función asincrónica que lea un archivo desde el sistema de archivos y devuelva el contenido como una cadena.
+
+
+// Crear una promesa que se rechace después de un tiempo determinado utilizando setTimeout().
+
+
+// Crear una función asincrónica que genere un número aleatorio y lo devuelva después de un tiempo determinado utilizando setTimeout().
+
+
+// Crear una función asincrónica que haga múltiples solicitudes a una API y devuelva los resultados en un arreglo.
+
+
+// Crear una función asincrónica que haga múltiples solicitudes a diferentes APIs y devuelva los resultados en un arreglo.
+
+
+// Crear una promesa que se resuelva si el usuario hace clic en un botón utilizando el evento click.
+
+
+// Crear una promesa que se resuelva si el usuario escribe algo en un campo de entrada utilizando el evento input.
+
+
+// Crear una función asincrónica que utilice fetch() para llamar a una API y devuelva los resultados en formato JSON.
+
+
+// Crear una función asincrónica que utilice XMLHttpRequest para llamar a una API y devuelva los resultados en formato JSON.
+
+
+// Crear una función asincrónica que lea varios archivos desde el sistema de archivos y devuelva el contenido en un arreglo.
+
+
+// Crear una promesa que se resuelva si el usuario permite el acceso a su ubicación utilizando navigator.geolocation.
+
+
+// Crear una promesa que se resuelva si el usuario permite el acceso a su cámara utilizando navigator.mediaDevices.getUserMedia().
+
+
+// Crear una promesa que se resuelva si el usuario permite el acceso a su micrófono utilizando navigator.mediaDevices.getUserMedia().
+
+
+// Crear una función asincrónica que utilice WebSockets para recibir mensajes en tiempo real de un servidor.
+
+
+// Crear una función asincrónica que utilice WebSockets para enviar mensajes a un servidor en tiempo real.
+
+
+// Crear una promesa que se resuelva si una imagen se carga correctamente utilizando el evento load.
+
+
+// Crear una promesa que se resuelva si una imagen no se carga correctamente utilizando el evento error.
+
+
+// Crear una función asincrónica que utilice IndexedDB para almacenar y recuperar datos en una base de datos local.
+
