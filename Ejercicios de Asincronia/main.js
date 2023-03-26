bc184b5c8f5fa89fc9caa7faa82430303d019821
RobinLB99 <RJLB991102@hotmail.com>
2023-03-26 00:46:16 -0500

    Ejercicios de asincronia

--- Ejercicios de Asincronia/main.js
+++ Ejercicios de Asincronia/main.js
@@ -46,14 +46,14 @@ printMessage()
 // Crear una función que use setInterval para imprimir un mensaje cada cierto tiempo.
 
 const printMessageConsole = () => {
-    return setInterval(function() {
+    return setInterval(function () {
         console.log('Este es un mensaje cada 10 segundos')
     }, 10000)
 }
 
 let intervalID = printMessageConsole()
 
-setTimeout(function() {
+setTimeout(function () {
     clearInterval(intervalID);
 }, 60000);
 
@@ -83,7 +83,7 @@ firstPromiseRevolve(promesas) // primero
 
 let promiseX = new Promise((resolve, reject) => {
     setTimeout(() => resolve("¡Hecho!"), 6000)
-  });
+});
 
 async function myAsyncFunction(promise) {
     let resultado = await promise
@@ -95,20 +95,122 @@ myAsyncFunction(promiseX)
 
 // Crear una función que use async/await y un bucle for para esperar a que varias promesas se resuelvan en secuencia.
 
+async function resolverPromesasEnSecuencia(promesas) {
+    const resultados = [];
+
+    for (let i = 0; i < promesas.length; i++) {
+        try {
+            const resultado = await promesas[i];
+            resultados.push(resultado);
+        } catch (error) {
+            console.error(`Promesa '${error}' rechazada`);
+        }
+    }
+
+    return console.log(resultados);
+}
+
+resolverPromesasEnSecuencia(promesas)
+
 
 // Crear una función que use async/await y Promise.all para esperar a que varias promesas se resuelvan en paralelo.
+
+async function isResolveAllPromiseInParallel(promesas) {
+    try {
+        const resultado = await Promise.all(promesas)
+        console.log(resultado); // Con Promise.all(), no se ejecutara si alguna promesa es rechazada.
+    } catch (error) {
+        console.log(`Promesa '${error}' rechazada`)
+    }
+}
+
+isResolveAllPromiseInParallel(promesas)
 
 
 // Crear una función que use fetch para obtener datos de una API y luego procese los datos recibidos.
 
+async function peticionDeUsuarios() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await response.json()
+        return console.log(users);
+    } catch (error) {
+        return console.error(error.message)
+    }
+}
+
+peticionDeUsuarios()
+
 
 // Crear una función que use XMLHttpRequest para obtener datos de una API y luego procese los datos recibidos.
+
+const XMLHttpRequestFunction = url => {
+    const peticion = new XMLHttpRequest()
+
+    peticion.open('GET', url)
+    peticion.send()
+
+    peticion.addEventListener('load', function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const data = JSON.parse(this.response)
+            return console.log(data)
+        }
+    })
+}
+
+XMLHttpRequestFunction('https://jsonplaceholder.typicode.com/comments')
 
 
 // Crear una función que maneje errores al obtener datos de una API usando fetch.
 
+async function dataAlbums() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums')
+        const albums = await response.json()
+        return console.log(albums);
+    } catch (error) {
+        return console.error(error.message)
+    }
+}
+
+dataAlbums()
+
 
 // Crear una función que maneje errores al obtener datos de una API usando XMLHttpRequest.
+
+const getDataFromAPI = (url /* , onSuccess, onError */) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load', function () {
+        if (this.readyState === 4 && this.status === 200) {
+            try {
+                const data = JSON.parse(this.response);
+                // onSuccess(data);
+                console.log(data);
+            } catch (error) {
+                // onError(error);
+                console.error(error.message);
+            }
+        } else {
+            console.error('Request failed')
+            // onError(new Error('Request failed'));
+        }
+    });
+
+    request.addEventListener('error', function () {
+        // onError(new Error('Network error'));
+        console.error('Network error')
+    });
+
+    request.open('GET', url);
+    request.send();
+};
+
+getDataFromAPI('https://jsonplaceholder.typicode.com/photos' /* ,
+data => console.log(data),
+error => console.error(error.message) */
+);
+
 
 
 // Crear una función que cancele una solicitud en curso usando AbortController.
